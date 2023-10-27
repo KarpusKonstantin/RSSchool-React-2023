ba50eb7ad31d6f39abff656b97edd37635177715
KarpusKonstantin <ks@wintecs.by>
2023-10-27 16:05:53 +0300

    feat: add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
+import List from './components/ListItems';
+import { axiosInstance } from './api/axios.api';
 
-function App() {
-  return <></>;
+class App extends React.Component {
+  async componentDidMount(): Promise<void> {
+    const { data } = await axiosInstance.get<[]>('');
+
+    console.log('DATA >>', data);
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <List />
+      </div>
+    );
+  }
 }
 
 export default App;
